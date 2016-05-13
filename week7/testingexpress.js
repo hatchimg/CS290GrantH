aad50c1df53c5eab3a5dbbab7f98e9e9c26ad50b
hatchimg <hatchimg@oregonstate.edu>
2016-05-13 13:24:53 -0400

    Add files via upload

--- week7/testingexpress.js
+++ week7/testingexpress.js
@@ -2,20 +2,20 @@ var express = require('express');
 
 var app = express();
 
-var handlebars = require('express-handlebars').create({defaultLayout:main});
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
-app.set('view engine' 'handlebars');
+app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 app.get('/getRequest',function(req,res){
 	var parameters = [];
 	for (var info in req.query){
-		parameters.push = {'attribute': info, 'value': req.query[info]}
+		parameters.push({'attribute': info, 'value': req.query[info]})
 	}
-	var theContext = {}
+	var theContext = {};
 	theContext.reqList = parameters;
-	res.render('get.handlebars, theContext');
+	res.render('get', theContext);
 });
 
 app.get('/other-page',function(req,res){
