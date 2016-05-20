1dd102da569176b7df8695e25a17cedf793b90f2
hatchimg <hatchimg@oregonstate.edu>
2016-05-19 22:40:33 -0400

    Add files via upload

---
+++ practice/testingscript.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+var handlebars = require("express-handlebars").create({defaultLayout:"main"});
+var session = require("express-sessions");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret = "password"}));
+
+app.set("view engine", "handlebars");
+app.set("port", "2000");
+
+app.get("/", function(req, res){
+	var context = {};
+	context.count = session.count || 0;
+	req.session.count = context.count + 1;
+
+	render("sessionstarted", context);
+});
