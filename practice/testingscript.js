ae916e50052c8280379222a65d7b7e6e32c7aae1
hatchimg <hatchimg@oregonstate.edu>
2016-05-22 14:11:38 -0400

    Add files via upload

--- practice/testingscript.js
+++ practice/testingscript.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
+var request = require("request");
 
 var handlebars = require("express-handlebars").create({defaultLayout:"main"});
 var session = require("express-session");
 var bodyParser = require("body-parser");
+var key = "a1b03c6d2cb7ce744200b7cc63f06240";
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,7 +22,7 @@ app.get("/", function(req, res, next){
 		return;
 	}
 	context.name = req.session.name;
-	context.count = req.session.toDo.length || 0;
+	context.toDoCount = req.session.toDo.length || 0;
 	context.toDo = req.session.toDo || [];
 	console.log(context.toDo);
 	res.render("sessionstarted", context);
@@ -29,7 +31,7 @@ app.get("/", function(req, res, next){
 app.post("/", function(req, res){
 	var context = {};
 	
-	if(req.body = ["submitName"]){
+	if(req.body["submitName"]){
 		req.session.name = req.body.name;
 		req.session.toDo = [];
 		req.session.curId = 0;
@@ -39,20 +41,31 @@ app.post("/", function(req, res){
 		res.render("homepage", context);
 		return;
 	}
-	if(req.body = ["submitNew"]){
-		req.session.toDo.push({"name": req.body.newItem, "id": req.session.curId})
+	if(req.body["submitNew"]){
+		
+		request("http://api.openweathermap.org/data/2.5/weather?q=" + req.body.itemCity + "&appid=" + key, function(err, response, body){
+			if(!err && res.statusCode < 400){
+				req.body.temp = body.main.temp;
+			} else{
+				if(response){
+					console.log(response.statusCode);
+				}
+				next(err);
+			}
+		});
+		req.session.toDo.push({"name": req.body.newItem, "id": req.session.curId, "temp": req.body.temp});
 		req.session.curId++;
 	}
 	
 	
-	if(req.body = ["Done"]){
+	if(req.body["Done"]){
 		req.session.toDo = req.session.toDo.filter(function(e){
 			return e.id != req.body.id;
 		})
 	}
 	
 	context.name = req.session.name;
-	context.count = req.session.toDo.length;
+	context.toDoCount = req.session.toDo.length;
 	context.toDo = req.session.toDo;
 	console.log(context.toDo);
 	res.render("sessionstarted", context);
