08e9a9132fc7963660238c1148b057a31a766e72
hatchimg <hatchimg@oregonstate.edu>
2016-05-25 21:31:32 -0400

    Add files via upload

--- CS 290 DB/db.js
+++ CS 290 DB/db.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var handlebars = require("express-handlebars").create({default layout: 'main'});
+var handlebars = require("express-handlebars").create({defaultLayout: 'main'});
 var request = require("request");
 
 var mysql = require("mysql");
@@ -14,9 +14,17 @@ var pool = mysql.createPool({
 
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", handlebars);
-app.set("port", 2500);
+app.set("port", 3306);
 
-app.get('/reset-table',function(req,res,next){
+app.get('/', function(req, res,next){
+	var context = {};
+	var table = document.getElementById("workoutTable");
+	var row = table.insertRow(0);
+	var cell1 = row.insertCell(0);
+	cell1.innerHTML = "This is a new cell";
+});
+
+/*app.get('/reset-table',function(req,res,next){
   var context = {};
   pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
@@ -31,7 +39,7 @@ app.get('/reset-table',function(req,res,next){
       res.render('home',context);
     })
   });
-});
+});*/
 
 app.listen(app.get("port"), function(){
 	console.log("Express started. Press Ctrl-C to terminate.");
