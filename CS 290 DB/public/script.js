fded9f690852cd0dc741652f7bb8a9a2da2754a0
hatchimg <hatchimg@oregonstate.edu>
2016-05-28 23:58:36 -0400

    Add files via upload

--- CS 290 DB/public/script.js
+++ CS 290 DB/public/script.js
@@ -1,4 +1,4 @@
-function addRow(){
+function addRow(event){
 
 	if(document.getElementById("name").value == ""){
 		alert("Name must be filled out.");
@@ -8,49 +8,61 @@ function addRow(){
 	var reps = document.getElementById("reps").value;
 	var weight = document.getElementById("weight").value;
 	var date = document.getElementById("date").value;
-	var type = document.getElementById("unit");
+	var type = document.getElementById("unit").value;
+	
+	var payload = {name: null, reps: null, weight: null, date: null, type: null};
+	payload.name = name;
+	payload.reps = reps;
+	payload.weight = weight;
+	payload.date = date;
+	payload.type = type;
 	
 	var req = new XMLHttpRequest();
-	req.open("GET", "http://localhost:2500/add-item?name=" + name + "&reps=" + reps + "&weight=" + weight + "&date=" + date + "&lbs=" + type, true);
-	req.send(null);
+	req.open("POST", "http://localhost:2500/add-item?name=" + name + "&reps=" + reps + "&weight=" + weight + "&date=" + date + "&lbs=" + type, true);
 	
-	if(req.status >= 200 && req.status < 400){
-	//add row to HTML table
-	
-	var newRow = document.createElement("TR");
+	req.addEventListener('load', function(){
+		if(req.status >= 200 && req.status < 400){
+			var res = JSON.parse(req.responseText);
+			for(p in res){
+				var newRow = document.createElement("TR");
 	
 	//name
 	var nameCell = document.createElement("TD");
-	nameCell.innerHTML = document.getElementById("name").value;
+	nameCell.innerHTML = p.name;
 	newRow.appendChild("nameCell")
 	
 	//reps
 	var repsCell = document.createElement("TD");
-	repsCell.innerHTML = document.getElementById("reps").value;
+	repsCell.innerHTML = p.reps;
 	newRow.appendChild("repsCell")
 	
 	//weight
 	var weightCell = document.createElement("TD");
-	weightCell.innerHTML = document.getElementById("weight").value;
+	weightCell.innerHTML = p.weight;
 	newRow.appendChild("weightCell")
 	
 	//date
 	var dateCell = document.createElement("TD");
-	dateCell.innerHTML = document.getElementById("date").value;
+	dateCell.innerHTML = p.date;
 	newRow.appendChild("dateCell")
 	
 	//type
 	var typeCell = document.createElement("TD");
-	typeCell.innerHTML = document.getElementById("type").value;
+	typeCell.innerHTML = p.type;
 	newRow.appendChild("typeCell")
 	
 	
 	getElementById("myTable").appendChild(newRow);
-	}
-	else{
-		console.log("Row not successfully added. Error code " + req.status);
-	}
+			}
+		}
+		else{
+			console.log("Error in network request: " + req.statusText);
+		}
+		
+	});
 	
-
-
-};
+	req.send(JSON.stringify(payload));
+	event.preventDefault();
+		
+	
+	};
