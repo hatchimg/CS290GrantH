d4cb69e91f214671a910787817ea5bc4a7984b4a
hatchimg <hatchimg@oregonstate.edu>
2016-05-28 20:14:32 -0400

    Add files via upload

--- CS 290 DB/dbandui.js
+++ CS 290 DB/dbandui.js
@@ -54,61 +54,7 @@ app.get("/add-item", function(req, res, next){
 	
 });
 
-function addRow(){
 
-	if(document.getElementById("name").value == ""){
-		alert("Name must be filled out.");
-		return;
-	}
-	var name = document.getElementById("name").value;
-	var reps = document.getElementById("reps").value;
-	var weight = document.getElementById("weight").value;
-	var date = document.getElementById("date").value;
-	var type = document.getElementById("unit");
-	
-	var req = new XMLHttpRequest();
-	req.open("GET", "http://52.32.212.47:2500/add-item?name=" + name + "reps=" + reps + "weight=" + weight + "&date=" + date + "&lbs=" + type, true);
-	req.send(null);
-	
-	if(req.status >= 200 && req.status < 400){
-	//add row to HTML table
-	
-	var newRow = document.createElement("TR");
-	
-	//name
-	var nameCell = document.createElement("TD");
-	nameCell.innerHTML = document.getElementById("name").value;
-	newRow.appendChild("nameCell")
-	
-	//reps
-	var repsCell = document.createElement("TD");
-	repsCell.innerHTML = document.getElementById("reps").value;
-	newRow.appendChild("repsCell")
-	
-	//weight
-	var weightCell = document.createElement("TD");
-	weightCell.innerHTML = document.getElementById("weight").value;
-	newRow.appendChild("weightCell")
-	
-	//date
-	var dateCell = document.createElement("TD");
-	dateCell.innerHTML = document.getElementById("date").value;
-	newRow.appendChild("dateCell")
-	
-	//type
-	var typeCell = document.createElement("TD");
-	typeCell.innerHTML = document.getElementById("type").value;
-	newRow.appendChild("typeCell")
-	
-	
-	getElementById("myTable").appendChild(newRow);
-	}
-	else{
-		console.log("Row not successfully added. Error code " + req.status);
-	}
-	
-
-}
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
